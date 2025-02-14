4be9a1a02f51d63f27b0d06d2d41d65ffc43c569
Ronald Marques <ronaldpmarques01@gmail.com>
2025-02-14 08:14:15 -0300

    Criação de modelo Investidor

---
+++ src/models/investor.py
@@ -0,0 +1,7 @@
+from src.drivers.database import db
+
+
+class Investor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
